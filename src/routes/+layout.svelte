260d618021886c9b3f13841beec67c2f43a3e694
Jeremy Mullis <jeremy.mullis@gmail.com>
2025-02-28 17:35:33 -0600

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+	import '../app.css';
+	import '@fontsource/zilla-slab/600.css';
+	import '@fontsource/podkova/600.css';
+	import AOS from 'aos';
+	import 'aos/dist/aos.css';
+	onMount(() => {
+		AOS.init();
+	});
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	// import Logo from '$lib/lytostech_logo.svg?component';
+	import Logo from '$lib/redheads-logo-lockup.png';
+	import Head from '$lib/redheads-logo-icon.png';
+
+	// import ContactButton from '$lib/lytostech_contact-button.svg?component';
+</script>
+
+<div class="lg:max-w-2xl max-w-lg mx-auto">
+	<div class="p-12">
+		<h1>
+			<!-- <Logo class="w-80 lg:w-96 h-auto mx-auto" /> -->
+			<img src={Logo} alt="redheads logo" />
+		</h1>
+		<div class="mt-12 space-y-6">
+			<p class="font-zilla">
+				So, ye think ye&#39;ve stumbled upon just another website? Think again. Let me tell you, the
+				tale of the RedHeads is a thing of fire and flavor, a tapestry woven with passion and a hint
+				of delightful swagger rooted deeply in the soil of this land.
+			</p>
+			<p class="font-zilla">
+				Aye, we&#39;re harvesting something grand, something that&#39;ll make yer taste buds sing
+				and yer spirits soar. Think of it as a bounty of legends, a gathering of the finest apples
+				the Eastern Stronghold has ever seen – and a glimpse into the very soul of us RedHeads.
+			</p>
+			<p class="font-zilla">
+				Don’t be surprised if we don’t play nice, and we definitely won’t hold back. We&#39;re
+				showing you what it means to be a RedHead, and what it means to own your legacy.
+			</p>
+			<p class="font-zilla">
+				So, bide yer time, eager souls. The gates to our orchard are about to swing wide, revealing
+				a world of flavor and tales that&#39;ll leave ye breathless. And trust me, it&#39;ll be
+				worth the wait.
+			</p>
+			<p data-aos="zoom-in" class="font-zilla text-2xl text-center text-red-700 uppercase">
+				Now, who wants to Bite a RedHead?
+			</p>
+			<p class="text-center">
+				<a
+					class="font-podkova hover:-translate-y-2 inline-block px-8 py-2 mx-auto text-2xl text-center text-white transition bg-red-700 rounded-full"
+					href="mailto:steve.wright@jpsullivan.com">I'll Bite</a
+				>
+			</p>
+		</div>
+		<img class="max-w-36 mx-auto mt-12" src={Head} alt="redheads logo" />
+	</div>
+</div>
+
+<!-- <enhanced:img
+	src="$lib/lytostech_bg.png"
+	alt="An alt text"
+	class="absolute inset-0 z-0 object-cover w-screen h-screen"
+/> -->
